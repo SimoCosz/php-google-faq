7146bfb72490cf45e73d55754c3bc474e598939f
coszach@hotmail.it <coszach@hotmail.it>
2022-03-30 13:40:52 +0200

    creata repositorie

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+  $faq = [
+    [
+      'domanda' => 'Come state implementando la recente decisione della Corte di giustizia dell\'Unione europea (CGUE) relativa al diritto all\'oblio?',
+      'risposta' => '',
+    ],
+    [
+      'domanda' => 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?',
+      'risposta' => '',
+    ],
+    [
+      'domanda' => 'Perché il mio account è associato a un paese?',
+      'risposta' => '',
+    ],
+    [
+      'domanda' => 'Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?',
+      'risposta' => '',
+    ],
+    [
+      'domanda' => 'Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?',
+      'risposta' => '',
+    ],
+  ]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php-google-faq</title>
+</head>
+<body>
+  
+</body>
+</html>
